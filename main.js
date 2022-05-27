5a78932e62fa0cb7f3870dcf533b0f6e44d069e3
Vishnu9412 <87561982+Vishnu9412@users.noreply.github.com>
2022-05-27 19:33:54 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs = require('hbs')
+
+const port = process.env.port || 3000
+
+const staticPath = path.join(__dirname, '/public')
+console.log(path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'hbs')
+app.use(express.static(staticPath))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}/`)
+    console.log(`Example app listening on port ${port}`)
+})
