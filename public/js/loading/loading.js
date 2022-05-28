a9c0dc4e79845c67520c1cb6ee44429c5679e079
Vishnu9412 <87561982+Vishnu9412@users.noreply.github.com>
2022-05-28 15:34:50 +0530

    loading screen created

--- public/js/loading/loading.js
+++ public/js/loading/loading.js
@@ -2,6 +2,8 @@ document.onreadystatechange = function() {
     if (document.readyState !== "complete") {
         setTimeout(fade, 1500);
         $('.loading-box').addClass('gone');
+        $('.loading-box span').fadeOut(1500);
+
 
         function fade() {
             $('.loading-box').fadeOut();
