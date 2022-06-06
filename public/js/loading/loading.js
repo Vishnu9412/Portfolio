6298d00885910a0a6ee39889e9b13e7f9b70ebd5
Vishnu9412 <87561982+Vishnu9412@users.noreply.github.com>
2022-06-06 12:48:13 +0530

    added video background

--- public/js/loading/loading.js
+++ public/js/loading/loading.js
@@ -1,13 +1,10 @@
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        setTimeout(fade, 1500);
-        $('.loading-box').addClass('gone');
-        $('.loading-box span').fadeOut(1000);
+$('body').ready(function() {
+    setTimeout(fade, 1500);
+    $('.loading-box').addClass('gone');
+    $('.loading-box span').fadeOut(1000);
 
+});
 
-        function fade() {
-            $('.loading-box').fadeOut();
-        }
-
-    }
-};
+function fade() {
+    $('.loading-box').fadeOut();
+}
